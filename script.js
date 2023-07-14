ab406f8386b8323236938062e6dbf73a84aba94f
Camp2507 <116607341+Camp2507@users.noreply.github.com>
2023-07-13 21:07:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function Enviar() {
+
+    var nome = document.getElementById("nomeid");
+
+    if (nome.value != "") {
+        alert('Sr(a) ' + nome.value + ' sua mensagem foi encaminhada com sucesso!');
+        
+    }
+
+}
